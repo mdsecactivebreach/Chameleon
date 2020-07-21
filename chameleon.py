72016f714308b37ad959b0306fb3ee682ca858f0
Wouter Bloeyaert <someniak@hotmail.com>
2020-07-21 04:56:07 -0700

    Added several changes to support updated API's and Python3

--- chameleon.py
+++ chameleon.py
@@ -2,38 +2,40 @@ import argparse
 import sys
 from modules import *
 
+
 class Chameleon:
 
     def __init__(self):
         pass
 
     def validate_args(self):
-        parser = argparse.ArgumentParser(description = "")
-        parser.add_argument("--proxy", metavar="<proxy>", dest = "proxy", default = None, help = "Proxy type: a = all, b = bluecoat, m = mcafee, i = IBM Xforce")
-        parser.add_argument("--check", action='store_true', help = "Perform check on current category")
-        parser.add_argument("--submit", action='store_true', help = "Submit new category")
-        parser.add_argument("--domain", metavar="<domain>", dest = "domain", default = None, help = "Domain to validate")
+        parser = argparse.ArgumentParser(description="")
+        parser.add_argument("--proxy", metavar="<proxy>", dest="proxy", default=None,
+                            help="Proxy type: a = all, b = bluecoat, m = mcafee, i = IBM Xforce")
+        parser.add_argument("--check", action='store_true', help="Perform check on current category")
+        parser.add_argument("--submit", action='store_true', help="Submit new category")
+        parser.add_argument("--domain", metavar="<domain>", dest="domain", default=None, help="Domain to validate")
         args = parser.parse_args()
 
         if not args.proxy:
-            print "[-] Missing --proxy argument"
+            print("[-] Missing --proxy argument")
             sys.exit(-1)
         if not args.domain:
-            print "[-] Missing --domain argument"
+            print("[-] Missing --domain argument")
             sys.exit(-1)
         if not args.check and not args.submit:
-            print "[-] Missing --check or --submit argument"
+            print("[-] Missing --check or --submit argument")
             sys.exit(-1)
         return args
 
     def show_banner(self):
         with open('banner.txt', 'r') as f:
             data = f.read()
-            print "\033[92m%s\033[0;0m" % data
+            print("\033[92m%s\033[0;0m" % data)
 
     def run(self, args):
         if args.proxy == 'm' or args.proxy == 'a':
-            print "\033[1;34m[-] Targeting McAfee Trustedsource\033[0;0m"
+            print("\033[1;34m[-] Targeting McAfee Trustedsource\033[0;0m")
             ts = trustedsource.TrustedSource(args.domain)
             if args.check:
                 ts.check_category(False)
@@ -41,16 +43,17 @@ class Chameleon:
                 ts.check_category(True)
 
         if args.proxy == 'b' or args.proxy == 'a':
-            print "\033[1;34m[-] Targeting Bluecoat WebPulse\033[0;0m"
+            print("\033[1;34m[-] Targeting Bluecoat WebPulse\033[0;0m")
             if args.check:
                 b = bluecoat.Bluecoat(args.domain, 'https://www.bankofamerica.com')
                 b.check_category()
             elif args.submit:
-                print "\033[1;31m[-] WARNING: This module must be run from the webserver you want to categorise\033[0;0m"
-                print "\033[1;31m[-] Proceed: Y/N\033[0;0m"
+                print(
+                    "\033[1;31m[-] WARNING: This module must be run from the webserver you want to categorise\033[0;0m")
+                print("\033[1;31m[-] Proceed: Y/N\033[0;0m")
                 while True:
-                    choice = raw_input().lower()
-                    if choice == 'Y' or choice =='y':
+                    choice = input().lower()
+                    if choice == 'Y' or choice == 'y':
                         b = bluecoat.Bluecoat(args.domain, 'https://www.bankofamerica.com')
                         b.run()
                         break
@@ -58,12 +61,13 @@ class Chameleon:
                         break
 
         if args.proxy == 'i' or args.proxy == 'a':
-            print "\033[1;34m[-] Targeting IBM Xforce\033[0;0m"
+            print("\033[1;34m[-] Targeting IBM Xforce\033[0;0m")
             xf = ibmxforce.IBMXforce(args.domain)
             if args.check:
                 xf.checkIBMxForce()
             elif args.submit:
                 xf.submit_category()
+
 
 if __name__ == "__main__":
     c = Chameleon()
--- modules/bluecoat.py
+++ modules/bluecoat.py
@@ -1,27 +1,27 @@
-
-import urllib2
-import requests
-import sys
-import re
-from urlparse import urlparse
-from bs4 import BeautifulSoup
+import http.server
 import json
+import re
+import socketserver
+import sys
 import threading
-import SocketServer
-import SimpleHTTPServer
+from urllib.parse import urlparse
 import time
+import traceback
+import requests
+import os
 
-
-class NewHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+class NewHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
             self.path = 'webroot/index.html'
-        return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
 
 class ThreadedHTTPServer(object):
     handler = NewHandler
+
     def __init__(self, host, port):
-        self.server = SocketServer.TCPServer((host, port), self.handler)
+        self.server = socketserver.TCPServer((host, port), self.handler)
         self.server_thread = threading.Thread(target=self.server.serve_forever)
         self.server_thread.daemon = True
 
@@ -32,6 +32,7 @@ class ThreadedHTTPServer(object):
         self.server.shutdown()
         self.server.server_close()
 
+
 class Bluecoat:
     def __init__(self, url, clonesite):
         self.url = url
@@ -39,64 +40,76 @@ class Bluecoat:
         self.server = ''
 
     def clone(self):
-        print "[-] Cloning " + self.clonesite
-        headers = { 'User-Agent' : 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1)' }
+        print("[-] Cloning " + self.clonesite)
+        headers = {'User-Agent': 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1)'}
         webContent = requests.get(self.clonesite, headers=headers).content
 
+        if not os.path.exists('webroot'):
+            os.makedirs('webroot')
+
         try:
-            if webContent.lower().index("<base href=\""):
+            if webContent.lower().index(b"<base href=\""):
                 pass
         except ValueError:
-            parsed_uri = urlparse( self.clonesite)
+            parsed_uri = urlparse(self.clonesite)
             base = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
-            content = re.sub(r"(<head.*?>)", "\g<0>\n<base href=\"" + base + "\">" , webContent, count=1, flags=re.IGNORECASE)
-            webContent = content
+            webContent = re.sub(b"(<head.*?>)", b"\g<0>\n<base href=\"" + bytes(base, encoding='utf8') + b"\">", webContent, count=1, flags=re.IGNORECASE)
 
-        with open('webroot/index.html', 'w') as indexFile:
+        with open('webroot/index.html', 'wb') as indexFile:
             indexFile.write(webContent)
             indexFile.close()
 
     def check_category(self):
         # Category checking lifted from CatMyFish
         # https://github.com/Mr-Un1k0d3r/CatMyFish/blob/master/CatMyFish.py
-        print "[-] Checking category for " + self.url
-        request = urllib2.Request("https://sitereview.bluecoat.com/resource/lookup")
-        request.add_header("User-Agent", "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1)")
-        request.add_header("Referer", "https://sitereview.bluecoat.com/lookup")
-        request.add_header("Content-Type", "application/json; charset=utf-8")
-        post_data = {"url": self.url, "captcha": ""} 
-        response = urllib2.urlopen(request, json.dumps(post_data))
+        print("[-] Checking category for " + self.url)
+
+        session = requests.session()
+        url = "https://sitereview.bluecoat.com/resource/lookup"
+        cookies = {"XSRF-TOKEN": "028e5984-50bf-4c00-ad38-87d19957201a"}
+        headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0",
+                         "Accept": "application/json, text/plain, */*", "Accept-Language": "en_US",
+                         "Accept-Encoding": "gzip, deflate", "Referer": "https://sitereview.bluecoat.com/",
+                         "X-XSRF-TOKEN": "028e5984-50bf-4c00-ad38-87d19957201a",
+                         "Content-Type": "application/json; charset=utf-8", "Connection": "close"}
+        data = {"captcha": "", "key": "",
+                      "phrase": "RXZlbiBpZiB5b3UgYXJlIG5vdCBwYXJ0IG9mIGEgY29tbWVyY2lhbCBvcmdhbml6YXRpb24sIHNjcmlwdGluZyBhZ2FpbnN0IFNpdGUgUmV2aWV3IGlzIHN0aWxsIGFnYWluc3QgdGhlIFRlcm1zIG9mIFNlcnZpY2U=",
+                      "source": "new lookup", "url": self.url}
+        response = session.post(url, headers=headers, cookies=cookies, json=data)
 
         try:
-            json_data = json.loads(response.read())
-            if json_data.has_key("errorType"):
+            json_data = json.loads(response.content)
+            if "errorType" in json_data:
                 if json_data["errorType"] == "captcha":
-                    print "[-] BlueCoat blocked us :("
+                    print("[-] BlueCoat blocked us :(")
                     sys.exit(0)
             category = []
             for entry in json_data["categorization"]:
                 category.append(entry["name"])
-            cat = ', '.join(category) 
-            print "\033[1;32m[-] Your site is categorised as: " + cat + "\033[0;0m"
+            cat = ', '.join(category)
+            print("\033[1;32m[-] Your site is categorised as: " + cat + "\033[0;0m")
         except Exception as e:
-			print "[-] An error occurred"
+            traceback.print_exc()
+
+            print("[-] An error occurred")
 
     def serve_content(self):
-        print "[-] Serving content over HTTP server"
-        self.server = ThreadedHTTPServer("0.0.0.0", 80)
-	try:
-        	self.server.start()
-	except:
-		pass
+        print("[-] Serving content over HTTP server")
+        self.server = ThreadedHTTPServer("0.0.0.0", 8000)
+        try:
+            self.server.start()
+        except:
+            pass
+
     def shutdown_server(self):
-        print "[-] Shutting down HTTP server"
+        print("[-] Shutting down HTTP server")
         self.server.stop()
 
     def run(self):
         self.clone()
         self.serve_content()
         time.sleep(10)
-	self.check_category()
+        self.check_category()
         self.shutdown_server()
 
 
